6a7d146121a64dfdec2f0ace606d7acdaab9e055
Pedro Ulisses <pedro.ulisses@aluno.uece.br>
2022-10-31 20:10:51 -0300

    Trabalhando com BD e Flask ao mesmo tempo

---
+++ serverBD.py
@@ -0,0 +1,8 @@
+#        Para esse novo tipo de server com Banco de Dados vamos estar precisando necessariamente das seguintes coisas : 
+#        Flask ( obviamente )
+#        flask_sqlalchemy ( Instalando através do pip ou Anaconda)
+#        mysql-connector-python
+#        mysqlclient
+#        
+#        Com isso pode estar dando progresso para o resto e no terminal : python3 , em seguida from <nome_do_arquivo> import db
+#        Esse comando so vai funcionar caso tenha todos os requisitos.
