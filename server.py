bf78462d4d6118f5580fb020268aa2a541589d66
Pedro Ulisses <pedro.ulisses@aluno.uece.br>
2022-10-31 17:33:13 -0300

    Colocando comentarios para melhor compreensão do codigo

--- server.py
+++ server.py
@@ -1,23 +1,26 @@
-#pip install flask -> instalar o flask
-from flask import Flask
+#conda install flask -> instalar com anaconda
+#pip install flask -> instalar o flask com o gerenciador de arquivos do Python
+from flask import Flask #Importando o Flask
 
 empregados = [
                 {'nome' : 'Valentina', 'cargo' : 'Analista' , 'salario' : 5000},
                 {'nome' : 'Joao', 'cargo' : 'Analista' , 'salario' : 3000},
                 {'nome' : 'Moises', 'cargo' : 'Garota de Programa' , 'salario' : 9999999}
-            ] #Dados ficticios
+            ] #Dados ficticios em modelo JSON
+#O motivo de usar JSON é por conta que o JSON é muito mais rápido comparado com o XML
+#Além da sua sintaxe ser simples e de compreensão facil
 
-app = Flask(__name__)
+app = Flask(__name__) #Construindo o APP
 
-@app.route("/")
+@app.route("/") #Decorator para a requisição da pagina inicial
 def home():
     return "<h1>Pagina Principal</h1>"
 
-@app.route("/empregados")
+@app.route("/empregados") #Decorator para a requisição da pagina com dados dos empregados
 def get_empregados():
-    return{'empregados': empregados}
+    return{'empregados': empregados} #Retorna todos os empregados
 
-@app.route("/empregados/<cargo>")
+@app.route("/empregados/<cargo>") #Decorator para requisição da pagina com dados dos empregados de acordo com o cargo
 def get_empregados_cargo(cargo):
     out_empregados = []
     for empregado in empregados:
@@ -25,7 +28,7 @@ def get_empregados_cargo(cargo):
             out_empregados.append(empregado)
     return {'empregados' : out_empregados}
 
-@app.route("/empregados/<info>/<value>")
+@app.route("/empregados/<info>/<value>") #Decorator para requisição da pagina empregados de acordo com a informação e o valor dela
 def get_empregados_info(info,value):
     out_empregados = []
     for empregado in empregados:
@@ -44,4 +47,4 @@ def get_empregados_info(info,value):
     return {'empregados': out_empregados}
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True) #Iniciando o aplicativo com debug para obter informações do servidor
