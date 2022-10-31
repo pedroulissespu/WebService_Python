ed11f9d2dbd2a073c267d05aaa1394c1d79713f5
Pedro Ulisses <pedro.ulisses@aluno.uece.br>
2022-10-31 16:25:06 -0300

    Webservice em Python , testando e mexendo com dados

---
+++ server.py
@@ -0,0 +1,29 @@
+#pip install flask -> instalar o flask
+from flask import Flask
+
+empregados = [
+                {'nome' : 'Valentina', 'cargo' : 'Analista' , 'salario' : 5000},
+                {'nome' : 'Joao', 'cargo' : 'Analista' , 'salario' : 3000},
+                {'nome' : 'Moises', 'cargo' : 'Garota de Programa' , 'salario' : 9999999}
+            ] #Dados ficticios
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "<h1>Pagina Principal</h1>"
+
+@app.route("/empregados")
+def get_empregados():
+    return{'empregados': empregados}
+
+@app.route("/empregados/<cargo>")
+def get_empregados_cargo(cargo):
+    out_empregados = []
+    for empregado in empregados:
+        if(cargo == empregado['cargo']):
+            out_empregados.append(empregado)
+    return out_empregados
+
+if __name__ == "__main__":
+    app.run(debug=True)
